dc570fe7fc590cbbb7f5328ac07cd174bfd40b93
João Füzessy <joao.fuzessy@dtidigital.com.br>
2019-08-02 03:00:00 -0300

    Function starting clock but still jumping at 60 seconds

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+
+
+setInterval(() => {
+    var date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    let hourPosition = 360/12*hr;
+    let minPosition = 360/60*min;
+    let secPosition = 360/60*sec;
+    HOURHAND.style.transform = "rotate(" + hourPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}, 1000);
